db76139eeaca935bb766c835062f2f0f0147ddd4
patry.dud <patry.dud@gmail.com>
2022-03-22 21:28:06 +0100

    final project

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -8,20 +8,37 @@ import { CommentService, Comment } from '../comment.service';
 })
 export class CommentComponent implements OnInit {
 
-  listaKomentow: Comment[] = [];
-  userComment = '';
-  commentId = 1000;
+  private _listaKomentow: Comment[] = [];
+  private _userComment = '';
+  private _commentId = 1000;
 
   constructor(private commentService:CommentService) { }
 
   ngOnInit(): void {
-      this.commentService.getComments().subscribe(response => this.listaKomentow = response);
+      this.commentService.getComments().subscribe(response => this._listaKomentow = response);
   }
 
   addComment(){
-    this.commentId++;
-    this.commentService.addComment({id: this.commentId, body: this.userComment, postId: 7})
+    this._commentId++;
+    this.commentService.addComment({id: this._commentId, body: this._userComment, postId: 7})
     .subscribe(resp => this.commentService.getComments()
-    .subscribe(response => this.listaKomentow = response));
+    .subscribe(response => this._listaKomentow = response));
   }
+
+  public get listaKomentow(){
+    return this._listaKomentow;
+  }
+
+  public get userComment(){
+    return this._userComment;
+  }
+
+  public set userComment(userComment: string){
+     this._userComment = userComment;
+  }
+
+  public get commentId(){
+    return this._listaKomentow;
+  }
+
 }
